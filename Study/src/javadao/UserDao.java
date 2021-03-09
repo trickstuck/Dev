b3c843044f789c39b85b43f355d371934dc3dbfa
men01 <men01@DESKTOP-VD9RB20>
2021-03-10 01:03:44 +0900

    trouble shooting.

--- Study/src/javadao/UserDao.java
+++ Study/src/javadao/UserDao.java
@@ -4,21 +4,6 @@ import java.sql.*;
 
 public class UserDao {
 
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection c = DriverManager.getConnection("jdbc:oracle:thin:@dalma.dongguk.ac.kr:1521:stud1", "trickstuck",
-				"ms1123");
-
-		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPw());
-
-		ps.executeUpdate();
-
-		ps.close();
-		c.close();
-	}
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -50,15 +35,14 @@ public class UserDao {
 		user.setName("백기선");
 		user.setPw("married");
 
-		dao.add(user);
+
 
 		System.out.println(user.getId() + " 등록 성공");
 
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPw());
+		System.out.println(user.getName());
+		System.out.println(user.getPw());
 
-		System.out.println(user2.getId() + " 조회 성공");
+		System.out.println(user.getId() + " 조회 성공");
 	}
 
 }
