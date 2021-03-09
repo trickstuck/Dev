e8a6eb2feb8f14f30a2f0b70eefab743f857640b
men01 <men01@DESKTOP-VD9RB20>
2021-03-09 22:25:07 +0900

    1st dao study

---
+++ Study/src/javadao/User.java
@@ -0,0 +1,31 @@
+package javadao;
+
+public class User {
+	String id;
+	String name;
+	String pw;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+}
