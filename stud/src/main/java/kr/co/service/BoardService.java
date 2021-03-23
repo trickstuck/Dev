953d81254e4136ceb3792d37541655d86125ef87
men01 <men01@DESKTOP-VD9RB20>
2021-03-24 06:14:00 +0900

    add Searching func

--- stud/src/main/java/kr/co/service/BoardService.java
+++ stud/src/main/java/kr/co/service/BoardService.java
@@ -3,7 +3,7 @@ package kr.co.service;
 import java.util.List;
 
 import kr.co.vo.BoardVO;
-import kr.co.vo.Criteria;
+import kr.co.vo.SearchCriteria;
 
 public interface BoardService {
 
@@ -11,10 +11,10 @@ public interface BoardService {
 	public void write(BoardVO boardVO) throws Exception;
 	
 	// 게시물 목록 조회
-	public List<BoardVO> list(Criteria cri) throws Exception;
+	public List<BoardVO> list(SearchCriteria scri) throws Exception;
 		
 	//게시물 총 갯수
-	public int listCount() throws Exception;
+	public int listCount(SearchCriteria scri) throws Exception;
 		
 	// 게시물 조회
 	public BoardVO read(int bno) throws Exception;
