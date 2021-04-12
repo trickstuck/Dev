5dc5f66172abb813bb31c4d6d0d9685867246250
men01 <men01@DESKTOP-VD9RB20>
2021-04-13 03:38:20 +0900

    04.13

--- stud/src/main/java/kr/co/vo/BoardVO.java
+++ stud/src/main/java/kr/co/vo/BoardVO.java
@@ -3,12 +3,53 @@ package kr.co.vo;
 import java.util.Date;
 
 public class BoardVO {
-
+	
 	private int bno;
 	private String title;
 	private String content;
 	private String writer;
 	private Date regdate;
+
+
+	private String lineno;
+	private String stname;
+	private String devide;
+	private String location;
+	private String call;
+	
+	
+	public String getLineno() {
+		return lineno;
+	}
+	public void setLineno(String lineno) {
+		this.lineno = lineno;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	public String getDevide() {
+		return devide;
+	}
+	public void setDevide(String devide) {
+		this.devide = devide;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getCall() {
+		return call;
+	}
+	public void setCall(String call) {
+		this.call = call;
+	}
+	
+	
 	
 	public int getBno() {
 		return bno;
@@ -40,5 +81,6 @@ public class BoardVO {
 	public void setRegdate(Date regdate) {
 		this.regdate = regdate;
 	}
+
 	
 }
